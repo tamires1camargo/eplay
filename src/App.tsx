bc4b63f6185e24e8cc5102e28e02018f797d366a
tamires_camargo <tamires.camargo.soares@gmail.com>
2023-08-28 18:18:33 -0300

    commit number three tamires

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
 
-const rotas = createBrowserRouter([
-  {
-    path: '/',
-    element: <Banner />
-  }
-])
+import Rotas from './routes'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <GlobalCss />
-      <div className="container">
-        <Header />
-      </div>
-      <RouterProvider router={rotas} />
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,104 @@
+import Banner from '../../components/Banner'
+import ProductList from '../../components/ProductList'
+import Game from '../../models/Game'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horr',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horr',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horr',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horr',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizaard Entertainament.',
+    title: 'Diablo 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizaard Entertainament.',
+    title: 'Zelda',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizaard Entertainament.',
+    title: 'Star Wars',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: starWars
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizaard Entertainament.',
+    title: 'Zelda',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  }
+]
+
+const Home = () => (
+  <>
+    <Banner />
+    <ProductList games={promocoes} title="Promoções" background="gray" />
+    <ProductList games={emBreve} title="Em breve" background="black" />
+  </>
+)
+
+export default Home
