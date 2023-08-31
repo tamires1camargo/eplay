6a995b71ce06428a22121091f87d239437fa58cf
tamires_camargo <tamires.camargo.soares@gmail.com>
2023-08-31 19:56:08 -0300

    commit number four tamires

---
+++ src/pages/Product/index.tsx
@@ -0,0 +1,44 @@
+import { useParams } from 'react-router-dom'
+import Hero from '../../components/Hero'
+import Section from '../../components/Section'
+
+import zelda from '../../assets/images/zelda.png'
+import Gallery from '../../components/Gallery'
+
+const Product = () => {
+  const { id } = useParams()
+
+  return (
+    <>
+      <Hero />
+      <Section title="Sobre o jogo" background="black">
+        <p>
+          Hogwarts Legacy é um RPG de ação imersivo e de mundo aberto no mundo
+          introduzido pela primeira nos livros do Harry Potter. Embarque em uma
+          jornada por locais novos e familiares enquanto explora e descubra
+          animais fantásticos, personalize seus personagens e crie poções,
+          domine o lançamento de feitiços, aprimore talentos e torne-se o bruxo
+          que deseja ser. Experimente Hogwarts da década de 1800. Seu personagem
+          é um estudante com chave de um antigo segredo que ameaça destruir o
+          mundo bruxo. Faça aliados, lute contra bruxos das trevas e decida o
+          destino do mundo. Seu legado é o que você faz dele. Viva o insperado.
+        </p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma:</b> PlayStation 5 <br />
+          <b>Desenvolvedor:</b> Avalanche Software <br />
+          <b>Editora:</b> Portkey Games, subsidiária da Warner Bros. Interative
+          Entertainment <br />
+          <b>Idiomas:</b> O jogo oferece suporte a diversos idiomas, incluindo
+          inglês, espanhol, francês, alemão, italiano, português, entre outros.
+          As opções de áudio e legendas podem ser ajustadas nas configurações do
+          jogo.
+        </p>
+      </Section>
+      <Gallery />
+    </>
+  )
+}
+
+export default Product
